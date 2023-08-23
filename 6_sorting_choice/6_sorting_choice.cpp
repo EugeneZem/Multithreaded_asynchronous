35f9016e4ec5f225b0548eacbc3ec90c6a8b3cf0
Eugene Zemlyanko <slayer0007@mail.ru>
2023-08-24 00:01:26 +0300

    edition4

---
+++ 6_sorting_choice/6_sorting_choice.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <future>
+#include <random>
+#include <list>
+#include <iterator>
+#include <algorithm>
+
+int const LIST_SIZE = 25;
+using LIST = std::list<int>;
+
+void generate_list(LIST& sorted_list, int LIST_SIZE)
+{
+	for (int i = 0; i < LIST_SIZE; ++i)
+		sorted_list.push_back(std::rand() % 90 + 10);
+}
+
+void print(LIST& sorted_list)
+{
+	for (auto pos : sorted_list)
+		std::cout << pos << "  ";
+	std::cout << std::endl;
+}
+
+LIST::iterator find_min(LIST& sorted_list, LIST::iterator current, std::promise<LIST::iterator> prom)
+{
+	LIST::iterator res = std::min_element(current, sorted_list.end());
+	prom.set_value(res);
+	return res;
+}
+
+void sorted(LIST& sorted_list)
+{
+	auto current = sorted_list.begin();
+	while (current != sorted_list.end())
+	{
+		std::promise<LIST::iterator> prom;
+		std::future<LIST::iterator> ft_mim = prom.get_future();
+		auto async_find = std::async(find_min, std::ref(sorted_list), current, std::move(prom));
+		LIST::iterator it_min = async_find.get();
+		std::swap(*current, *it_min);
+		++current;
+	}
+}
+
+int main()
+{
+	LIST sorted_list;
+	generate_list(sorted_list, LIST_SIZE);
+	print(sorted_list);
+	sorted(sorted_list);
+	print(sorted_list);
+	return 0;
+}
---
+++ 7_for_each/7_for_each.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <algorithm>
+#include <future>
+#include <random>
+
+template <typename It, typename Funct>
+void my_for_each(It begin, It end, Funct& funct, int lim_size = 50)
+{
+	int hardware_cores = std::thread::hardware_concurrency();
+	int size = std::distance(begin, end) / hardware_cores;
+	if (size <= lim_size)
+	{
+		std::for_each(begin, end, &funct);
+		return;
+	}
+	auto mdl = begin;
+	std::advance(mdl, size / 2);
+	auto sync = std::async(my_for_each<It, Funct>, begin, mdl, std::ref(funct), lim_size);
+	my_for_each(begin, mdl, funct, lim_size);
+	return;
+}
+
+void test_function(int& i)
+{
+
+	i = std::rand() % 90 + 10;
+}
+
+template <typename T>
+void print(T& data)
+{
+	for (auto pos : data)
+		std::cout << pos << "  ";
+	std::cout << std::endl;
+}
+
+int main()
+{
+	std::vector<int> test (200);
+
+	print(test);
+
+	my_for_each(test.begin(), test.end(), test_function);
+
+	print(test);
+}
